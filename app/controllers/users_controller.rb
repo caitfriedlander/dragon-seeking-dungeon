55b48d5c5a2b628f4a750749acc1dc0decc98431
Cait Friedlander <caitfinchart@gmail.com>
2018-07-15 20:50:19 -0700

    created user search page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,6 @@ class UsersController < ApplicationController
       @user = User.new(user_params)
       if @user.save
         session[:user_id] = @user.id
-        flash[:notice] = "You have signed up!"
         redirect_to users_path
       else 
         render :new
@@ -29,7 +28,15 @@ class UsersController < ApplicationController
     end
   
     def update
-
+      respond_to do |format|
+        if @user.update(user_params)
+          format.html { redirect_to @user}
+          format.json { render :show, status: :ok, location: @user }
+        else
+          format.html { render :edit }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
+      end
     end
   
     def destroy
