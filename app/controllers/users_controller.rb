cf745ffb210fc3bec31d51247a137a9f795a2f1b
Cait Friedlander <caitfinchart@gmail.com>
2018-07-20 10:00:07 -0700

    fixed signup bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-    before_action :authorize, except: [:show]
+    before_action :authorize, except: [:show, :new]
 
     def index
       @users = User.all
